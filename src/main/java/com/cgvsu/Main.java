0a6cc53414f63ad7c8f0439adca985004eba5c98
fetr <fetr9415694@gmail.com>
2023-12-28 19:26:37 +0300

    modify

--- src/main/java/com/cgvsu/Main.java
+++ src/main/java/com/cgvsu/Main.java
@@ -3,7 +3,5 @@ package com.cgvsu;
 public class Main {
     public static void main(String[] args) {
         Simple3DViewer.main(args);
-        Simple3DViewer.main(args);
-        Simple3DViewer.main(args);
     }
 }
